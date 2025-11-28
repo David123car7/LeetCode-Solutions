c7cf3499108d2b148d3679001bc12353eec65f58
CrZ <81176282+David123car7@users.noreply.github.com>
2025-11-28 10:30:31 +0000

    238_ProductExceptSelf

---
+++ CSharp/238_ProductExceptSelf.cs
@@ -0,0 +1,62 @@
+/*
+ * LeetCode Problem: Product of Array Except Self (238)
+ * URL: https://leetcode.com/problems/product-of-array-except-self/
+ * Difficulty: Medium
+ *
+ * Description:
+ * Given an integer array nums, return an array answer such that answer[i] is equal to
+ * the product of all the elements of nums except nums[i].
+ *
+ * Goal:
+ * Calculate the product of all other elements for each index without using division
+ * and in O(n) time.
+ *
+ * Key Insight:
+ * The product of "all elements except self" is mathematically equivalent to:
+ * (Product of all elements to the Left) * (Product of all elements to the Right).
+ *
+ * The Strategy:
+ * 1. Prefix Array: Create an array where index 'i' contains the product of all numbers
+ * from index 0 up to 'i'.
+ * 2. Suffix Array: Create an array where index 'i' contains the product of all numbers
+ * from the end of the array down to 'i'.
+ * 3. Result Construction:
+ * - For the first element: Take the suffix of the next element.
+ * - For the last element: Take the prefix of the previous element.
+ * - For middle elements: Multiply prefix[i-1] * suffix[i+1].
+ *
+ * Time Complexity: O(N)
+ * - We perform three distinct linear passes over the array.
+ *
+ * Space Complexity: O(N)
+ * - We use two auxiliary arrays (prefix and suffix) of size N.
+ */
+public int[] ProductExceptSelf(int[] nums) {
+    int[] prefix = new int[nums.Length];
+    int[] sufix = new int[nums.Length];
+    int[] result = new int[nums.Length];
+
+    prefix[0] = nums[0];
+    for(int i=1; i<nums.Length; i++){
+        prefix[i] = nums[i] * prefix[i-1];
+    }
+
+    sufix[nums.Length-1] = nums[nums.Length-1];
+    for(int i=nums.Length-2; i>=0; i--){
+        sufix[i] = nums[i] * sufix[i+1];
+    }
+
+    for(int i=0; i<nums.Length; i++){
+        if(i==0){
+            result[i] = sufix[i+1];
+        }
+        else if(i==nums.Length-1){
+            result[i] = prefix[i-1]; 
+        }
+        else{
+            result[i] = prefix[i-1] * sufix[i+1] ;
+        }
+    }
+
+    return result;
+}
